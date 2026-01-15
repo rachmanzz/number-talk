d237cabcac0f75158f11c90ab77876a3947e2b8c
Muhammad Abdurrahman <rachman.sd@gmail.com>
2026-01-15 14:20:20 +0800

    fix: adjust server initialization to check runtime type before serving

--- number-backend/src/index.ts
+++ number-backend/src/index.ts
@@ -1,15 +1,20 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { runtime } from './runtime.js'
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.json({"helo": "work"})
+  return c.json({ "helo": "work" })
 })
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+if (runtime.type !== "cloudflare-worker") {
+  serve({
+    fetch: app.fetch,
+    port: 3000
+  }, (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`)
+  })
+}
+
+export default app
