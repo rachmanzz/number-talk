06dc41eaf7c3c88897566cf45b9cfad050b51930
Muhammad Abdurrahman <rachman.sd@gmail.com>
2026-01-15 14:28:50 +0800

    worker testing case

--- number-backend/src/index.ts
+++ number-backend/src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.json({ "helo": "work" })
+  return c.json({ "helo": "yes, is work" })
 })
 
 export default app
