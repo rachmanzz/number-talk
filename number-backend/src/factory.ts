fd87bef905cf7c6a3a3fd932c74222b12306231f
Muhammad Abdurrahman <rachman.sd@gmail.com>
2026-01-15 22:49:11 +0800

    feat(backend): add SECRET_SIGN to Hono bindings

--- number-backend/src/factory.ts
+++ number-backend/src/factory.ts
@@ -1,7 +1,8 @@
 import { createFactory } from 'hono/factory'
 
 type Bindings = {
-  DB: D1Database
+  DB: D1Database,
+  SECRET_SIGN: string
 }
 
 export const factory = createFactory<{ Bindings: Bindings }>()
--- number-backend/src/index.ts
+++ number-backend/src/index.ts
@@ -4,7 +4,8 @@ import { HandleCreateUser } from './handlers/users.handler.js'
 const app = factory.createApp()
 
 app.get('/', async (c) => {
-  return c.json({type: "cloudflare worker", act: "handle apis"})
+
+  return c.json({type: "cloudflare worker", act: "handle apis", bid: c.env.SECRET_SIGN})
 })
 
 app.post('/auth/user/register', ...HandleCreateUser)
