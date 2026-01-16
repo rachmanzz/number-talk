91d6dd0286e7b7751978eb04357b0b062c0ab62a
Muhammad Abdurrahman <rachman.sd@gmail.com>
2026-01-16 21:21:44 +0800

    feat: node query

---
+++ number-frontend/src/apis/node/query.ts
@@ -0,0 +1,30 @@
+import { useQuery } from "@tanstack/react-query"
+import { backendUrl } from "../../utils/backend"
+import { responseSchema } from "../response.schema"
+
+export interface NodeTable {
+    id: string
+    parent_id?: string
+    user_username: string
+    operation: string
+    left_value: number
+    right_value: number
+    result_value: number
+    created_at: string
+    children?: NodeTable[]
+} 
+
+export const  useGetTreeNodes = () => {
+    return useQuery({
+        queryKey: ["treenodeskey"],
+        queryFn: async (): Promise<NodeTable[]> => {
+            const resp = await fetch(backendUrl("/open/node-trees"))
+            const rawJsonResult = await resp.json()
+            const rawZodResult = responseSchema.safeParse(rawJsonResult)
+            if (rawZodResult.success && Array.isArray(rawZodResult.data.data)) {    
+                return rawZodResult.data.data
+            }
+            return []
+        },
+    })
+}
