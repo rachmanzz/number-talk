1c9df117bc0d7a79539a098435d9282849a4a8b6
Muhammad Abdurrahman <rachman.sd@gmail.com>
2026-01-16 14:01:45 +0800

    feat(backend): add APP_STAGE to Hono bindings

--- number-backend/src/factory.ts
+++ number-backend/src/factory.ts
@@ -3,6 +3,7 @@ import { createFactory } from 'hono/factory'
 type Bindings = {
   DB: D1Database,
   SECRET_SIGN: string
+  APP_STAGE: string
 }
 
 export const factory = createFactory<{ Bindings: Bindings }>()
