44ed3cc7eb0ea481ecd5caa89793b8f0e1371459
Muhammad Abdurrahman <rachman.sd@gmail.com>
2026-01-16 21:26:56 +0800

    fix: remove not used resources

--- number-frontend/src/apis/node/mutation.ts
+++ number-frontend/src/apis/node/mutation.ts
@@ -1,4 +1,4 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { useMutation } from "@tanstack/react-query"
 import { backendUrl } from "../../utils/backend"
 import { responseSchema } from "../response.schema"
 import { toast } from "react-toastify"
