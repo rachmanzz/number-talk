5f2ce3e6cc5d1b83a745ac3c52caa8e00a29f1d6
Muhammad Abdurrahman <rachman.sd@gmail.com>
2026-01-16 21:21:30 +0800

    feat: node mutation

---
+++ number-frontend/src/apis/node/mutation.ts
@@ -0,0 +1,40 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { backendUrl } from "../../utils/backend"
+import { responseSchema } from "../response.schema"
+import { toast } from "react-toastify"
+
+export type OperationType = 'start' | 'add' | 'sub' | 'mul' | 'div'
+type numberPostType = {
+    operation: OperationType
+    val: string
+    parent_id?: string
+}
+
+
+export const usePostNumber = (whenSuccess: ()=> void) => {
+    return useMutation({
+        mutationFn: async (data: numberPostType) => {
+            return fetch(backendUrl("authorized/node/operation"), {
+                method: "POST",
+                body: JSON.stringify(data),
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                credentials: "include"
+            }).then(r => r.json())
+        },
+        onSuccess(data) {
+            const resp = responseSchema.safeParse(data)
+            if (resp.success) {
+                if (resp.data.success) {
+                    toast("Login success")
+                    whenSuccess()
+                } else {
+                    toast(resp.data.message ?? "Fail post a number", { type: "error" })
+                }
+            } else {
+                toast("uknown error, please contact the administrator", { type: "error" })
+            }
+        },
+    })
+}
