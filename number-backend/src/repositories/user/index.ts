ce0891c444c05cc58a6569f244f159bc8f8c088c
Muhammad Abdurrahman <rachman.sd@gmail.com>
2026-01-16 14:18:58 +0800

    feat: add user repository

--- number-backend/src/repositories/user/index.ts
+++ number-backend/src/repositories/user/index.ts
@@ -3,6 +3,9 @@ import type { DatabaseType } from "../database.js";
 import type { NewUser } from "./schema/user.schema.js";
 
 export const createUser = async (db: Kysely<DatabaseType>, col: NewUser) => {
-    return db.insertInto("users").values(col).execute()
+    return db.insertInto("users").values(col).executeTakeFirstOrThrow()
 }
 
+export const getUserByUsername = async (db: Kysely<DatabaseType>, val: string) => {
+    return db.selectFrom("users").selectAll().where("username", "==", val).executeTakeFirst()
+}
