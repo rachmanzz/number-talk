8d5252377a98cdfac373a698f3f49543edaa90fd
Muhammad Abdurrahman <rachman.sd@gmail.com>
2026-01-16 21:17:58 +0800

    feat: update database type

--- number-backend/src/repositories/database.ts
+++ number-backend/src/repositories/database.ts
@@ -1,5 +1,7 @@
+import type { NodeTable } from "./nodes/schema/node.schema.js"
 import type { UserTable } from "./user/schema/user.schema.js"
 
 export type DatabaseType = {
-    users: UserTable
+    users: UserTable,
+    node_trees: NodeTable
 }
